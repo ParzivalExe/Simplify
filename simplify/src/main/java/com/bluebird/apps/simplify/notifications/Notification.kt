74efd9e5b7efb9450a5b44c2658f7cdfaf5e5ecc
Lasse Hartmann <latech.tech@gmail.com>
2020-02-01 23:56:39 +0100

    small development update (errors removed + showNotification)

--- simplify/src/main/java/com/bluebird/apps/simplify/notifications/Notification.kt
+++ simplify/src/main/java/com/bluebird/apps/simplify/notifications/Notification.kt
@@ -2,15 +2,14 @@ package com.bluebird.apps.simplify.notifications
 
 import android.app.Activity
 import android.app.Notification
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
+import android.os.Bundle
 import androidx.core.app.NotificationCompat
-import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 class Notification(var context: Context, var smallIcon: Int) {
 
@@ -27,7 +26,7 @@ class Notification(var context: Context, var smallIcon: Int) {
         this.messageText =  messageText
     }
 
-    fun toNotificationCompat(notificationId: Int) : Notification {
+    fun toNotificationCompat() : Notification {
         var builder = NotificationCompat.Builder(context).apply {
             setSmallIcon(smallIcon)
             setContentTitle(messageTitle)
@@ -49,6 +48,10 @@ class Notification(var context: Context, var smallIcon: Int) {
         return builder.build()
     }
 
+    fun showNotification(notificationId: Int, manager: NotificationManager) {
+        manager.notify(notificationId, toNotificationCompat())
+    }
+
 
 
 
@@ -61,7 +64,7 @@ class Notification(var context: Context, var smallIcon: Int) {
         var actionIcon: Int
         var actionText: String
         var context: Context
-        var extras: HashMap<String, String> = hashMapOf()
+        var intentExtras: Bundle? = null
 
 
         constructor(actionClass: Class<Object>, actionId: String, actionIcon: Int, actionText: String, context: Context) {
@@ -76,25 +79,25 @@ class Notification(var context: Context, var smallIcon: Int) {
             this.context = context
         }
 
-        constructor(actionClass: Class<Object>, actionId: String, actionIcon: Int, actionText: String, context: Context, extras: HashMap<String, String>) : this(actionClass, actionId, actionIcon, actionText, context) {
-            this.extras = extras
+        constructor(actionClass: Class<Object>, actionId: String, actionIcon: Int, actionText: String, context: Context, extras: Bundle) : this(actionClass, actionId, actionIcon, actionText, context) {
+            this.intentExtras = extras
         }
 
         fun getIntent(): PendingIntent? {
             if(activity != null) {
                 val intent = Intent(context, activity).apply {
                     action = actionId
-                }
-                for(extraKey in extras.keys) {
-                    intent.putExtra(extraKey, extras[extraKey])
+                    if(intentExtras != null) {
+                        putExtras(intentExtras!!)
+                    }
                 }
                 return PendingIntent.getActivity(context, 0, intent, 0)
             }else if(broadcastReceiver != null) {
                 val intent = Intent(context, broadcastReceiver).apply {
                     action = actionId
-                }
-                for(extraKey in extras.keys) {
-                    intent.putExtra(extraKey, extras[extraKey])
+                    if(intentExtras != null) {
+                        putExtras(intentExtras!!)
+                    }
                 }
                 return PendingIntent.getBroadcast(context, 0, intent, 0)
             }
